09d6a915884d1601d57b687b4b582b0330328fda
tntgamez <mainman002@yahoo.com>
2024-02-15 12:02:59 -0600

    First add of project files to server

---
+++ src/main.cc
@@ -0,0 +1,59 @@
+#include <SDL.h>
+
+int main(int argc, char* argv[]) {
+    // Initialize SDL2
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
+
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 800, SDL_WINDOW_SHOWN);
+    if (!window) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to create window: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // Create a renderer for the window
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!renderer) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to create renderer: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Set render color to sky blue
+    SDL_SetRenderDrawColor(renderer, 135, 206, 235, 255);
+
+    // Clear background color
+    SDL_RenderClear(renderer);
+
+    // Set render color to red
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+    // Draw a red square
+    SDL_Rect rect = { 100, 100, 50, 50 };
+    SDL_RenderFillRect(renderer, &rect);
+
+    // Update the window
+    SDL_RenderPresent(renderer);
+
+    // Event loop
+    bool quit = false;
+    SDL_Event event;
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+    }
+
+    // Clean up
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
